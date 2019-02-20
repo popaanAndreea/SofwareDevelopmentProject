22bfc077bf7157f4eb753210d0bd6756776d7e34
Sam <sammw98@gmail.com>
2019-02-19 20:35:15 -0500

    Created Sam's Branch

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -1,7 +1,7 @@
 /**
  * SYST 17796 Project Winter 2019 Base code.
  * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+ * Sam's Branch
  */
 package ca.sheridancollege.project;
 
